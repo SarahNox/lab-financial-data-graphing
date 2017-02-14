9a2f74124c8ced895d5714b511d9ee35bccbee39
SarahNox <apnoeist@googlemail.com>
2017-02-14 16:04:50 +0100

    first part

---
+++ starter-code/public/javascripts/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $.ajax({
+    url: "http://api.coindesk.com/v1/bpi/historical/close.json",
+    method: "GET",
+    success: function (response) {
+      var bitCoinData = JSON.parse(response).bpi;
+      console.log(bitCoinData);
+      var chartData = [];
+      var chartLabels = [];
+      var ctx = $("#myChart");
+      for (var date in bitCoinData) {
+        chartData.push(bitCoinData[date]);
+        chartLabels.push(date);
+      }
+      var chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          datasets: [{
+            data: chartData
+          }],
+          labels: chartLabels
+        },
+      });
+    },
+    error: function (err) {
+      console.log(err);
+    },
+  })
+});
